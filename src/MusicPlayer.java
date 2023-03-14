5657927b0255315ff8365a2a1838175cac0b3645
pletena5 <17karasimov_m@bexleygs.co.uk>
2023-03-14 09:25:08 +0000

    Computer science IA

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,4 +1,5 @@
 //importing all necessary packages
+/*
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 
@@ -9,8 +10,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 
+
+
+
 //implementing ActionListener interface
 public class MusicPlayer implements ActionListener {
+
+
     JFrame frame;
     JLabel songNameLabel=new JLabel();
     JButton selectButton=new JButton("Select Mp3");
@@ -40,8 +46,8 @@ public class MusicPlayer implements ActionListener {
         frame=new JFrame();
         frame.setTitle("Music Player");
         frame.getContentPane().setLayout(null);
-        frame.getContentPane().setBackground(Color.pink);
-        frame.setSize(440,200);
+        frame.getContentPane().setBackground(Color.CYAN);
+        frame.setSize(1920,1080);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -85,9 +91,9 @@ public class MusicPlayer implements ActionListener {
             fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
             fileChooser.setFileFilter(new FileNameExtensionFilter("Mp3 files","mp3"));
             if(fileChooser.showOpenDialog(selectButton)==JFileChooser.APPROVE_OPTION){
-                myFile=fileChooser.getSelectedFile();
-                filename=fileChooser.getSelectedFile().getName();
-                filePath=fileChooser.getSelectedFile().getPath();
+                File myFile = new File("C:\\Users\\17karasimov_m\\IdeaProjects\\Computer science IA\\Mario kart music.mp3");
+                filename=myFile.getName();
+                filePath=myFile.getPath();
             }
         }
         if(e.getSource()==playButton){
@@ -162,3 +168,6 @@ public class MusicPlayer implements ActionListener {
         }
     };
 }
+
+
+ */
