10dff74a8549c6a119b22667dd2e8cefca1dc19a
pletena5 <17karasimov_m@bexleygs.co.uk>
2023-07-11 09:24:27 +0100

    excel function added

--- src/ExcelFormat.java
+++ src/ExcelFormat.java
@@ -1,7 +1,15 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-//import org.apache.*;
+import org.apache.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
 
 public class ExcelFormat {
 
@@ -9,7 +17,12 @@ public class ExcelFormat {
 
         try {
             FileInputStream file = new FileInputStream(new File(location));
-            //Workbook workbook = new XSSFWorkbook(file);
+            HSSFWorkbook workbook = new HSSFWorkbook(file);
+
+            HSSFSheet sheet= workbook.getSheetAt(0);
+
+
+
         }catch(IOException e){
             e.printStackTrace();
         }// exception handler
